7227771cea39de69f1ecec94b9bfa1bcd8ff09d2
mekuwamoto <eiji.mek@gmail.com>
2023-06-11 17:25:22 -0300

    Created base interfaces for services

---
+++ src/Onion.Template.Application/Commom/Interfaces/Authentication/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onion.Template.Application.Commom.Interfaces.Authentication;
+
+public interface IAuthenticationService
+{
+    string Register(string firstName, string lastName, string email);
+}

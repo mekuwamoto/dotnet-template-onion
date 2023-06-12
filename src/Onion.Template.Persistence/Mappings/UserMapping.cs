58fce4e088edab0a1936fa3a0541c5028fe6c8f6
mekuwamoto <eiji.mek@gmail.com>
2023-06-11 22:02:33 -0300

    Created mappings for first entity

---
+++ src/Onion.Template.Persistence/Mappings/UserMapping.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Onion.Template.Domain.Entities;
+
+namespace Onion.Template.Persistence.Mappings;
+
+public class UserMapping : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("TB_USER");
+        builder.HasKey(p => p.Id);
+        builder
+            .Property(p => p.Id)
+            .HasColumnName("ID_USER");
+        builder
+            .Property(p => p.FirstName)
+            .HasColumnName("DS_FIRST_NAME");
+        builder
+            .Property(p => p.FirstName)
+            .HasColumnName("DS_LAST_NAME");
+        builder
+            .Property(p => p.Email)
+            .HasColumnName("DS_EMAIL");
+        builder
+            .Property(p => p.Email)
+            .HasColumnName("DS_PWD");
+    }
+}
--- src/Onion.Template.Persistence/UserContext.cs
+++ src/Onion.Template.Persistence/UserContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Onion.Template.Domain.Entities;
+using Onion.Template.Persistence.Mappings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +12,13 @@ namespace Onion.Template.Persistence
 {
     public class UserContext : DbContext
     {
-        public UserContext(DbContextOptions<UserContext> options) : base(options)
-        {
-
-        }
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }

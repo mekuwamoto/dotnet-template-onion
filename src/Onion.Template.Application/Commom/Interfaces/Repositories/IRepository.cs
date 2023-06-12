56dc9707ae9e548042a9fdc5ccaaac815b4e300c
mekuwamoto <eiji.mek@gmail.com>
2023-06-11 22:48:54 -0300

    Created base interface for repositories

---
+++ src/Onion.Template.Application/Commom/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onion.Template.Application.Commom.Interfaces.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<T> GetByIdAsync(long id);
+    Task<string> AddAsync(T entity);
+    Task<string> UpdateAsync(T entity);
+    Task<string> DeleteAsync(long id);
+}

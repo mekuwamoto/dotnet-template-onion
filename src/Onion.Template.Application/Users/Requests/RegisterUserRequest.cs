00bf9e9f8a8b6d9c026a12616386e6132ac3c8b3
mekuwamoto <eiji.mek@gmail.com>
2023-06-27 22:11:23 -0300

    feat(api): RegisterUser data annotation

--- src/Onion.Template.Application/Users/Requests/RegisterUserRequest.cs
+++ src/Onion.Template.Application/Users/Requests/RegisterUserRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace Onion.Template.Application.Users.Requests;
 
 public class RegisterUserRequest
 {
+    [Required]
     public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
+	[Required]
+	public string LastName { get; set; } = null!;
+	[Required, EmailAddress]
+	public string Email { get; set; } = null!;
+	[Required]
+	public string Password { get; set; } = null!;
 }

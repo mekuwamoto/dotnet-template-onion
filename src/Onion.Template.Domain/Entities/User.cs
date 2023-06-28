006dc6b3493e8af26dc7986f0bd76a6b450f0c48
mekuwamoto <eiji.mek@gmail.com>
2023-06-27 22:59:12 -0300

    feat(domain): updated propeties and constructor user entity

--- src/Onion.Template.Domain/Entities/User.cs
+++ src/Onion.Template.Domain/Entities/User.cs
@@ -9,11 +9,20 @@ namespace Onion.Template.Domain.Entities;
 public class User
 {
 	public Guid Id { get; set; } = Guid.NewGuid();
-	public string FirstName { get; set; } = null!;
-	public string LastName { get; set; } = null!;
-	public string Email { get; set; } = null!;
-	public string Username { get; set; } = null!;
-	public string PasswordSalt { get; set; } = null!;
-	public string PasswordHash { get; set; } = null!;
+	public string FirstName { get; private set; } = null!;
+	public string LastName { get; private set; } = null!;
+	public string Email { get; private set; } = null!;
+	public string Username { get; private set; } = null!;
+	public string PasswordSalt { get; private set; } = null!;
+	public string PasswordHash { get; private set; } = null!;
 
+	public User(string firstName, string lastName, string email, string username, string passwordSalt, string passwordHash)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		Email = email;
+		Username = username;
+		PasswordSalt = passwordSalt;
+		PasswordHash = passwordHash;
+	}
 }

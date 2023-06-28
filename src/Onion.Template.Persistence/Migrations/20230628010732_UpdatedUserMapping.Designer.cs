ba812d72b409f9c6a07db4cbf890f4667a680032
mekuwamoto <eiji.mek@gmail.com>
2023-06-27 22:08:58 -0300

    feat(persistence): Add migrations user entity change

--- src/Onion.Template.Persistence/Migrations/20230628010732_UpdatedUserMapping.Designer.cs
+++ src/Onion.Template.Persistence/Migrations/20230628010732_UpdatedUserMapping.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Onion.Template.Persistence.Contexts;
 
@@ -11,9 +12,11 @@ using Onion.Template.Persistence.Contexts;
 namespace Onion.Template.Persistence.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20230628010732_UpdatedUserMapping")]
+    partial class UpdatedUserMapping
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Onion.Template.Persistence/Migrations/20230628010732_UpdatedUserMapping.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Onion.Template.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedUserMapping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DS_PWD",
+                table: "TB_USER",
+                newName: "DS_USERNAME");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DS_PWD_HASH",
+                table: "TB_USER",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DS_PWD_SALT",
+                table: "TB_USER",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DS_PWD_HASH",
+                table: "TB_USER");
+
+            migrationBuilder.DropColumn(
+                name: "DS_PWD_SALT",
+                table: "TB_USER");
+
+            migrationBuilder.RenameColumn(
+                name: "DS_USERNAME",
+                table: "TB_USER",
+                newName: "DS_PWD");
+        }
+    }
+}

e4fd868f22e1a420cd9ce0eb5b31f721610bf8be
mekuwamoto <eiji.mek@gmail.com>
2023-06-27 22:23:47 -0300

    feat(application): Created interface for password hasher

---
+++ src/Onion.Template.Application/Commom/Interfaces/Authentication/IPwdHasher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onion.Template.Application.Commom.Interfaces.Authentication;
+
+public interface IPwdHasher
+{
+	string ComputeHash(string password, string salt, string pepper, int iteration);
+	string GenerateSalt();
+}

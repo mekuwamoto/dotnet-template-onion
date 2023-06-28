c065d88a7b286dc9df7ab2a1a55a68b675611bed
mekuwamoto <eiji.mek@gmail.com>
2023-06-27 23:23:35 -0300

    feat(application): Finished returning registerresponse

--- src/Onion.Template.Application/Users/Commands/Register/Register.cs
+++ src/Onion.Template.Application/Users/Commands/Register/Register.cs
@@ -1,8 +1,10 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using Microsoft.Extensions.Options;
 using Onion.Template.Application.Commom.Interfaces.Authentication;
 using Onion.Template.Application.Commom.Settings;
 using Onion.Template.Application.Users.Requests;
+using Onion.Template.Application.Users.Response;
 using Onion.Template.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -13,30 +15,33 @@ using System.Threading.Tasks;
 
 namespace Onion.Template.Application.Users.Commands.Register;
 
-public struct RegisterCommand : IRequest<string>
+public struct RegisterCommand : IRequest<RegisterUserResponse>
 {
 	public RegisterCommand(RegisterUserRequest user) => User = user;
 	public RegisterUserRequest User { get; set; }
 }
 
-public class RegisterHandler : IRequestHandler<RegisterCommand, string>
+public class RegisterHandler : IRequestHandler<RegisterCommand, RegisterUserResponse>
 {
 	private readonly IPwdHasher _hasher;
 	private readonly HashSettings _settings;
+	private readonly IMapper _mapper;
 
-	public RegisterHandler(IPwdHasher wdHasher, IOptions<HashSettings> settings)
+	public RegisterHandler(IPwdHasher wdHasher, IOptions<HashSettings> settings, IMapper mapper)
 	{
 		_hasher = wdHasher;
 		_settings = settings.Value;
+		_mapper = mapper;
 	}
 
-	public async Task<string> Handle(RegisterCommand command, CancellationToken cancellationToken)
+	public async Task<RegisterUserResponse> Handle(RegisterCommand command, CancellationToken cancellationToken)
 	{
 		string salt = _hasher.GenerateSalt();
 		string hash = _hasher.ComputeHash(command.User.Password, salt, _settings.Pepper, _settings.Iterations);
 
 		User user = new User(command.User.FirstName, command.User.LastName, command.User.Email, command.User.Username, salt, hash);
 
-		return "";
+		RegisterUserResponse userResponse = _mapper.Map<RegisterUserResponse>(user);
+		return userResponse;
 	}
 }

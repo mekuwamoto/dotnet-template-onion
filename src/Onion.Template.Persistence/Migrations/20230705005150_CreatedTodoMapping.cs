d2bc8c5bd447a4bae1d5974eb1a2a46059051152
mekuwamoto <eiji.mek@gmail.com>
2023-07-04 21:54:14 -0300

    feat: Created migration for todo entity

---
+++ src/Onion.Template.Persistence/Migrations/20230705005150_CreatedTodoMapping.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Onion.Template.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedTodoMapping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_TODO",
+                columns: table => new
+                {
+                    ID_TODO = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ID_USEER = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DS_TITLE = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FL_COMPLETED = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_TODO", x => x.ID_TODO);
+                    table.ForeignKey(
+                        name: "FK_TB_TODO_TB_USER_ID_USEER",
+                        column: x => x.ID_USEER,
+                        principalTable: "TB_USER",
+                        principalColumn: "ID_USER",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TB_TODO_ID_USEER",
+                table: "TB_TODO",
+                column: "ID_USEER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_TODO");
+        }
+    }
+}

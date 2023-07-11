4bec9a06ca6388ce0b39c1506c302bc5528fcdf8
mekuwamoto <eiji.mek@gmail.com>
2023-07-10 22:11:27 -0300

    test(application): Login feature tests

---
+++ tests/UnitTests/Onion.Template.Application.UnitTests/Users/Fixtures/UsersFixtures.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using Onion.Template.Application.Commom.Settings;
+using Onion.Template.Application.Users.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onion.Template.Application.UnitTests.Users.Fixtures;
+
+public static class UsersFixtures
+{
+	public static LoginRequest GetLoginRequest()
+	{
+		var loginRequest = new LoginRequest()
+		{
+			Email = "email@email.com",
+			Password = "pwd123"
+		};
+		return loginRequest;
+	}
+
+	public static IOptions<HashSettings> GenerateHashSettingsConfig()
+	{
+		IOptions<HashSettings> config = Options.Create(new HashSettings()
+		{
+			Iterations = 5,
+			Pepper = "77f234c8-618e-4028-9aa1-b4851c60d6c8"
+		});
+		return config;
+	}
+
+	public static User GenerateGenericUser()
+	{
+		return new User(
+			"firstName",
+			"lastName",
+			"firsName.lastName@email.com",
+			"first",
+			"YWxcmfS4GWG2U3Ai8WkOcg==",
+			"cZdnYHY79cCBQzPoSvYChUMcU8iGUYh6rC2C1/PbdWk="
+		);
+	}
+}

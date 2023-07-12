aa6d4f9e03cef82d1425a4069a98faa250142b5d
mekuwamoto <eiji.mek@gmail.com>
2023-07-12 20:41:01 -0300

    test(application): CreateTodoCommand testing

--- tests/UnitTests/Onion.Template.Application.UnitTests/Todos/Factories/TodoFactory.cs
+++ tests/UnitTests/Onion.Template.Application.UnitTests/Todos/Factories/TodoFactory.cs
@@ -1,4 +1,6 @@
-﻿using Onion.Template.Application.Todos.Response;
+﻿using Onion.Template.Application.Todos.Commands.CreateTodo;
+using Onion.Template.Application.Todos.Requests;
+using Onion.Template.Application.Todos.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,10 @@ namespace Onion.Template.Application.UnitTests.Todos.Factories;
 public static class TodoFactory
 {
 	private static string DefaultTitle => "Task todo";
-	private static Guid DefaultGuid => new Guid("2da2d889-fa7b-e8b3-4302-ac37153a1bb4");
+	private static Guid DefaultUserId => new Guid("2da2d889-fa7b-e8b3-4302-ac37153a1bb4");
 
 	public static Todo CreateNewTask()
-		=> new Todo(DefaultTitle, DefaultGuid);
+		=> new Todo(DefaultTitle, DefaultUserId);
 
 	public static Todo CreateCompletedTask()
 	{
@@ -31,5 +33,18 @@ public static class TodoFactory
 			Title = todo.Title,
 			Completed = todo.Completed
 		};
+	}
+
+	public static CreateTodoRequest CreateTodoRequest()
+	{
+		return new CreateTodoRequest()
+		{
+			Title = DefaultTitle,
+		};
+	}
+
+	public static CreateTodoCommand CreateTodoCommand()
+	{
+		return new CreateTodoCommand(CreateTodoRequest());
 	}
 }
